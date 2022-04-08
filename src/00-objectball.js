000c4577f561a3a7cab4b97e85049d88e89b04ec
jesse zenon <jesse.zenon@gmail.com>
2022-04-08 09:34:42 -0500

    commit tree 515d6a09917bae8d639c8fbfe753938d0ddd9f81

---
+++ src/00-objectball.js
@@ -0,0 +1,24 @@
+const gameObject = {
+  home: {
+    teamName: ["Brooklyn Nets"],
+    colors: ["Black", "White"],
+    players: [
+      "Alan Anderson",
+      "Reggie Evans",
+      "Brook Lopez",
+      "Mason Plumlee",
+      "Jason Terry",
+    ],
+  },
+  away: {
+    teamName: ["Charlotte Hornets"],
+    colors: ["Turquoise", "Purple"],
+    players: [
+      "Jeff Adrien",
+      "Bismak Biyombo",
+      "Desagna Diop",
+      "Ben Gorden",
+      "Brendan Haywood",
+    ],
+  },
+};
